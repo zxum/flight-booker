0bc03fdd37af97c82ebd80ea87cbab08a9bbf571
zxum <zion.um@outlook.com>
2020-11-18 15:46:33 -0600

    Fixed Flight Index Page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -10,11 +10,13 @@ class FlightsController < ApplicationController
 
     if params[:day].blank? && params[:month].blank? && params[:year].blank?
       if params[:from_airport_id].blank? && params[:to_airport_id].blank?
-        @matched_flights = Flight.all
+        @matched_flights = nil
       elsif params[:from_airport_id].blank?
         @matched_flights = Flight.where(to_airport_id: params[:to_airport_id])
       elsif params[:to_airport_id].blank?
         @matched_flights = Flight.where(from_airport_id: params[:from_airport_id])
+      elsif params[:to_airport_id] == params[:from_airport_id]
+        redirect_to flights_path, alert: "Please select a different destination from your origin."
       else
         @matched_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id])
       end
@@ -22,15 +24,30 @@ class FlightsController < ApplicationController
       @matched_date = Date.civil(params[:year].to_i, params[:month].to_i, params[:day].to_i)
       if params[:from_airport_id].blank? && params[:to_airport_id].blank?
         @matched_flights = Flight.where(time: @matched_date.all_day)
+        flight_available(@matched_flights)
       elsif params[:from_airport_id].blank?
         @matched_flights = Flight.where(to_airport_id: params[:to_airport_id],time: @matched_date.all_day)
+        flight_available(@matched_flights)
       elsif params[:to_airport_id].blank?
         @matched_flights = Flight.where(from_airport_id: params[:from_airport_id],time: @matched_date.all_day)
+        flight_available(@matched_flights)
+      elsif params[:to_airport_id] == params[:from_airport_id]
+        redirect_to flights_path, alert: "Please select a different destination from your origin."
       else
         @matched_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], time: @matched_date.all_day)
+        flight_available(@matched_flights)
       end
     else
       redirect_to flights_path, alert: "Something went wrong. Please try again."
+    end
+  end
+
+  private 
+  def flight_available(flights)
+    if flights.empty? 
+      redirect_to flights_path, alert: "There are no flights available for your selection, please search again."
+    else
+      flights
     end
   end
 end
