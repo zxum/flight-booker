16884327ece1d219efc305177068d763afd6ebb9
zxum <zion.um@outlook.com>
2020-11-20 16:27:54 -0600

    Fix mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => "salty-bastion-40242.herokuapp.com"}
+  config.action_mailer.default_url_options = { :host => "salty-bastion-40242.herokuapp.com" }
 
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
